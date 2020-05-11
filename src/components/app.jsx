6526925e5d8f05bc744bd80f5ecf1b864a8599be
RAM JANAM YADAV <ramjanamyadavnitk.181ec136@nitk.edu.in>
2020-05-11 14:14:42 +0530

    updated components

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Card from "./card";
 import Contacts from "../contacts";
+import Avatar from "./avatar";
 function App(){
     return <div>
     <h1>My Contacts</h1>
+    <Avatar image="https://i.pinimg.com/236x/04/56/86/0456869906dfa906c494b2b63aa67f2a.jpg"/>
     <Card name=  {Contacts[0].name}
           image= {Contacts[0].image}
           phone= {Contacts[0].phone}
--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Avatar(props){
-    return <img src={props.image} alt="avatar_img"/>
+    return <img className="circle_img" src={props.image} alt="avatar_img"/>
 }
 
 export default Avatar;
